5eec7b2bf2c7ec42beb29543d0c0bcf4bcd65da8
tingli1991 <litingxian@live.cn>
2018-08-23 11:08:20 +0800

    新增log4net和nlong的扩展

---
+++ src/DMS.Core/DMS.Log4net/Extensions/Log4Context.cs
@@ -0,0 +1,49 @@
+﻿using log4net;
+using log4net.Repository;
+using System.IO;
+
+namespace DMS.Log4net
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Log4Context
+    {
+        /// <summary>
+        /// 系统日志记录器
+        /// </summary>
+        protected internal static ILog SysLog { get; private set; }
+
+        /// <summary>
+        /// 程序异常日志记录器
+        /// </summary>
+        protected internal static ILog ExceptionLog { get; private set; }
+
+        /// <summary>
+        /// 基础服务日志记录器
+        /// </summary>
+        protected internal static ILog WaLiuBasicServiceLog { get; private set; }
+
+        /// <summary>
+        /// API监控日志
+        /// </summary>
+        protected internal static ILog ApiMonitorLog { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="configPath"></param>
+        public static void Configure(string configPath)
+        {
+            FileInfo file = new FileInfo(configPath);
+            ILoggerRepository repository = LogManager.CreateRepository("NETCoreRepository");
+            log4net.Config.XmlConfigurator.ConfigureAndWatch(repository, file);
+
+            SysLog = LogManager.GetLogger(repository.Name, "SystemLogger");
+            ExceptionLog = LogManager.GetLogger(repository.Name, "ExceptionLogger");
+            WaLiuBasicServiceLog = LogManager.GetLogger(repository.Name, "WaLiuBasicServiceLogger");
+            ApiMonitorLog = LogManager.GetLogger(repository.Name, "ApiMonitorLogger");
+            SysLog.Info($"初始化{configPath}完成。");
+        }
+    }
+}
---
+++ src/DMS.Core/DMS.Log4net/Layout/CustomLayout.cs
@@ -0,0 +1,61 @@
+﻿using log4net.Core;
+using log4net.Layout;
+using log4net.Layout.Pattern;
+using System.IO;
+using System.Reflection;
+
+namespace DMS.Log4net.Layout
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class LogMsgLayout : PatternLayout
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public LogMsgLayout()
+        {
+            AddConverter("property", typeof(LogMessagePatternConverter));
+        }
+    }
+
+    /// <summary>
+    /// 模式转换
+    /// </summary>
+    public class LogMessagePatternConverter : PatternLayoutConverter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="writer"></param>
+        /// <param name="loggingEvent"></param>
+        protected override void Convert(TextWriter writer, LoggingEvent loggingEvent)
+        {
+            if (Option != null)
+            {
+                WriteObject(writer, loggingEvent.Repository, LookupProperty(Option, loggingEvent));
+            }
+            else
+            {
+                WriteDictionary(writer, loggingEvent.Repository, loggingEvent.GetProperties());
+            }
+        }
+
+        /// <summary>
+        /// 通过反射获取传入的日志对象的某个属性的值
+        /// </summary>
+        /// <param name="property"></param>
+        /// <param name="loggingEvent"></param>
+        /// <returns></returns>
+        private object LookupProperty(string property, LoggingEvent loggingEvent)
+        {
+            object propertyValue = string.Empty;
+            PropertyInfo propertyInfo = loggingEvent.MessageObject.GetType().GetProperty(property);
+            if (propertyInfo != null)
+                propertyValue = propertyInfo.GetValue(loggingEvent.MessageObject, null);
+
+            return propertyValue;
+        }
+    }
+}
