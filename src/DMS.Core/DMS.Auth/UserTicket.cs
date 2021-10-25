b705a0ecc6477705d185760c1422a857fd8e91d6
hailang2ll <79522860@qq.com>
2021-10-25 18:07:07 +0800

    删除不要的项目

--- src/DMS.Core/DMS.Auth/UserTicket.cs
+++ src/DMS.Core/DMS.Auth/UserTicket.cs
@@ -5,7 +5,15 @@ namespace DMS.Auth
     [Serializable]
     public class UserTicket
     {
+        /// <summary>
+        /// 主键ID
+        /// </summary>
+        /// <value></value>
         public long ID { get; set; } = 0;
+        /// <summary>
+        /// 名称
+        /// </summary>
+        /// <value></value>
         public string Name { get; set; }
         public DateTime ExpDate { get; set; } = DateTime.Now.AddDays(90);
         public string Msg { get; set; } = "初始化";
