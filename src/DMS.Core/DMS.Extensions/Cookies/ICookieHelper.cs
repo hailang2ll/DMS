d264b60b6d3c93fa113b417923ab88cb01e170d3
hailang2ll <79522860@qq.com>
2022-01-14 16:59:51 +0800

    新增cookies中间件

---
+++ src/DMS.Core/DMS.Extensions/Cookies/ICookieHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DMS.Extensions.Cookies
+{
+    public interface ICookieHelper
+    {
+        void SetCookie(string key, string value);
+
+        void SetCookie(string key, string value, int expiresTime);
+
+        string GetCookie(string key);
+
+        void DeleteCookie(string key);
+    }
+}
