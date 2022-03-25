fe46f1258498d7519dbc0fd8da7131dab1f0b38e
hailang2ll <79522860@qq.com>
2022-03-25 18:06:55 +0800

    加入仓库1

---
+++ samples/net6.0/DMS.Sample.Contracts/IBaseServices.cs
@@ -0,0 +1,25 @@
+﻿using DMS.Common.Model.Result;
+using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DMS.Sample.Contracts
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public interface IBaseServices<TEntity> where TEntity : class
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="objId"></param>
+        /// <returns></returns>
+        Task<TEntity> QueryById(object objId);
+
+    }
+}
---
+++ samples/net6.0/DMS.Sample.Service/BaseServices.cs
@@ -0,0 +1,26 @@
+﻿using DMS.Sample.Contracts;
+using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DMS.Sample.Service
+{
+    public class BaseServices<TEntity> : IBaseServices<TEntity> where TEntity : class, new()
+    {
+        public ISqlSugarClient _db;
+        public BaseServices(ISqlSugarClient sqlSugar)
+        {
+            _db = sqlSugar;
+        }
+
+        public async Task<TEntity> QueryById(object objId)
+        {
+            return await _db.Queryable<TEntity>().InSingleAsync(objId);
+           
+        }
+    }
+
+}
