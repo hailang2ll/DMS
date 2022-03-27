8a43d9573782e619b353b90834f469a62467556f
hailang2ll <79522860@qq.com>
2022-03-27 18:50:16 +0800

    加入工作单元

---
+++ src/DMS.Repository/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SqlSugar;
+
+namespace SqlSugar.Template.Repository.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        SqlSugarClient GetDbClient();
+
+        void BeginTran();
+
+        void CommitTran();
+        void RollbackTran();
+    }
+}
---
+++ src/DMS.Repository/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,48 @@
+﻿namespace SqlSugar.Template.Repository.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ISqlSugarClient _sqlSugarClient;
+        private readonly ITenant _tenant;
+
+        public UnitOfWork(ISqlSugarClient sqlSugarClient)
+        {
+            _sqlSugarClient = sqlSugarClient;
+            _tenant = _sqlSugarClient as ITenant;
+        }
+
+        /// <summary>
+        /// 获取DB，保证唯一性
+        /// </summary>
+        /// <returns></returns>
+        public SqlSugarClient GetDbClient()
+        {
+            // 必须要as，后边会用到切换数据库操作
+            return _sqlSugarClient as SqlSugarClient;
+        }
+
+        public void BeginTran()
+        {
+            _tenant.BeginTran();
+        }
+
+        public void CommitTran()
+        {
+            try
+            {
+                _tenant.CommitTran(); 
+            }
+            catch (Exception ex)
+            {
+                _tenant.RollbackTran();
+                //Logger.Error($"{ex.Message}\r\n{ex.InnerException}");
+            }
+        }
+
+        public void RollbackTran()
+        {
+            _tenant.RollbackTran();
+        }
+
+    }
+}
