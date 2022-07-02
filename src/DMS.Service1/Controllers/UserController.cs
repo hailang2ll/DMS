3e521c839dbc84f246e153fd3699df5ac3746428
hailang2ll <79522860@qq.com>
2022-07-02 16:02:47 +0800

    a

---
+++ src/DMS.Service1/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using DMS.Common.ControllerExt;
+using DMS.Common.Model.Result;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DMS.Service1.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : BaseApiController
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("Hello")]
+        public IActionResult Hello()
+        {
+            return Content("我是user1");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("GetUser")]
+        public ResponseResult GetUser(long id)
+        {
+            ResponseResult result = new ResponseResult()
+            {
+                data = id,
+                errmsg = "我是user1",
+            };
+            return result;
+        }
+    }
+}
---
+++ src/DMS.Service2/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using DMS.Common.ControllerExt;
+using DMS.Common.Model.Result;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DMS.Service2.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : BaseApiController
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("Hello")]
+        public IActionResult Hello()
+        {
+            return Content("我是user2");
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("GetUser")]
+        public ResponseResult GetUser(long id)
+        {
+            ResponseResult result = new ResponseResult()
+            {
+                data = id,
+                errmsg = "我是user2",
+            };
+            return result;
+        }
+    }
+}
